b8641e6b33fbb112a73ac2eb6b1aeb8446e4b359
Ninja Solution <top1successfulman@gmail.com>
2023-08-20 05:21:52 +1000

    update

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(cors());
 
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', allowedOrigins[0]);
+  res.setHeader('Access-Control-Allow-Origin', "*");
   // another common pattern
   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
